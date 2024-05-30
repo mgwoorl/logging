38f849a9b51532b944da246c078faf43c4734b20
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-31 01:24:00 +0800

    Update user_service.py

--- user_service.py
+++ user_service.py
@@ -9,6 +9,23 @@ import uvicorn
 
 app = FastAPI()
 
+FORMATTER_STRING = "%(asctime)s — %(name)s — %(levelname)s — %(message)s"
+FORMATTER = logging.Formatter(FORMATTER_STRING)
+LOG_FILE = "logging_broker.log" 
+
+def get_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.INFO)
+    
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(FORMATTER)
+    logger.addHandler(console_handler)
+
+    file_handler = TimedRotatingFileHandler(LOG_FILE, when='midnight')
+    logger.addHandler(file_handler)
+
+    return logger
+
 @app.get("/auth")
 async def gen_id():
     user_id = hashlib.md5(str(datetime.datetime.now()).encode('utf-8')).hexdigest()
